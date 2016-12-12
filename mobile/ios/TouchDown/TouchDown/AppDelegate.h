04248738d4765d6f7f65e2973d855bc90f5aa340
Eranjan <eranjan@calcey.com>
2016-12-13 01:57:43 +0530

    Created a new iOS project and added Firebase cocoapod.

---
+++ mobile/ios/TouchDown/TouchDown/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TouchDown
+//
+//  Created by Eranjan Punchihewa on 12/13/16.
+//  Copyright © 2016 Calcey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
